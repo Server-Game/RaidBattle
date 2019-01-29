a56598a09139cb89de45ae528b17ead253497ad8
杉山　涼 <sugisugirr@cg2160091.da.iwasaki.ac.jp>
2019-01-29 09:04:18 +0900

    PUNの制御を追加

--- RaidBattle/Assets/Resources/Script/API/PhotonManager.cs
+++ RaidBattle/Assets/Resources/Script/API/PhotonManager.cs
@@ -6,20 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class PhotonManager : Photon.MonoBehaviour
 {
+	[SerializeField]
+	Button roomButton;
+
+	private List<string> userNames;
+
 	public void Start()
 	{
+		userNames = new List<string>();
+		
 		DontDestroyOnLoad(this.gameObject);
+		ConnectFhoton();
+		OnJoinedLobby();
 	}
 
 	public void ConnectFhoton()
     {
-        PhotonNetwork.ConnectUsingSettings("v1.0");
+        var flag = PhotonNetwork.ConnectUsingSettings("v1.0");
 
+		if (flag) Debug.Log("<color=Blue>Success Connect PUN");
+		else Debug.LogError("<color=red>Faild Connect PUN");
     }
 
     public void OnJoinedLobby()
     {
-        Debug.Log("<color=red>PhotonManager OnJoinedLobby</color>");
+        Debug.Log("<color=Blue>PhotonManager OnJoinedLobby</color>");
         GameObject.Find("CreateRoom").GetComponent<Button>().interactable = true;
         GameObject.Find("JoinRoom").GetComponent<Button>().interactable = true;
 
@@ -28,6 +39,7 @@ public class PhotonManager : Photon.MonoBehaviour
     //ルーム作成
     public void CreateRoom()
     {
+		// TODO サーバから引っ張る
         string userName = "ユーザ1";
         string userId = "user1";
 
@@ -40,12 +52,12 @@ public class PhotonManager : Photon.MonoBehaviour
         PhotonNetwork.SetPlayerCustomProperties(customProp);
 
         RoomOptions roomOptions = new RoomOptions();
-        roomOptions.customRoomProperties = customProp;
+        roomOptions.CustomRoomProperties = customProp;
         //ロビーで見えるルーム情報としてカスタムプロパティのuserName,userIdを使いますよという宣言
-        roomOptions.customRoomPropertiesForLobby = new string[] { "userName", "userId" };
-        roomOptions.maxPlayers = 2; //部屋の最大人数
-        roomOptions.isOpen = true; //入室許可する
-        roomOptions.isVisible = true; //ロビーから見えるようにする
+        roomOptions.CustomRoomPropertiesForLobby = new string[] { "userName", "userId" };
+        roomOptions.MaxPlayers = 4; //部屋の最大人数
+        roomOptions.IsOpen = true; //入室許可する
+        roomOptions.IsVisible = true; //ロビーから見えるようにする
         //userIdが名前のルームがなければ作って入室、あれば普通に入室する。
         PhotonNetwork.JoinOrCreateRoom(userId, roomOptions, null);
     }
@@ -64,10 +76,11 @@ public class PhotonManager : Photon.MonoBehaviour
             //ルームが1件以上ある時ループでRoomInfo情報をログ出力
             for (int i = 0; i < rooms.Length; i++)
             {
-                Debug.Log("RoomName:" + rooms[i].name);
-                Debug.Log("userName:" + rooms[i].customProperties["userName"]);
-                Debug.Log("userId:" + rooms[i].customProperties["userId"]);
-                GameObject.Find("StatusText").GetComponent<Text>().text = rooms[i].name;
+				userNames.Add(rooms[i].Name);
+                Debug.Log("RoomName:" + rooms[i].Name);
+                Debug.Log("userName:" + rooms[i].CustomProperties["userName"]);
+                Debug.Log("userId:" + rooms[i].CustomProperties["userId"]);
+                GameObject.Find("StatusText").GetComponent<Text>().text = rooms[i].Name;
             }
         }
     }
@@ -79,7 +92,8 @@ public class PhotonManager : Photon.MonoBehaviour
     //ルーム入室した時に呼ばれるコールバックメソッド
     public void OnJoinedRoom()
     {
-        Debug.Log("PhotonManager OnJoinedRoom");
+        Debug.Log("<color=red>PhotonManager OnJoinedRoom</color>");
+      
         GameObject.Find("StatusText").GetComponent<Text>().text = "ルームに入室しました";
 
         GameObject.Find("Photon").GetComponent<Button>().interactable = false;
@@ -91,5 +105,23 @@ public class PhotonManager : Photon.MonoBehaviour
     {
 
     }
+
+	private void OnGUI()
+	{
+		
+		GUI.Button(new Rect(20, 20, 100, 40), "ルーム一覧");
+
+		if(userNames.Count == 0) 
+			GUI.Button(new Rect(30, 80, 100, 40), "ルームなし");
+
+		for (int i = 0; i < userNames.Count; i++)
+		{
+			if (i < 5)      GUI.Button(new Rect(30,  20 + (60 * (i + 1)), 100, 40), userNames[i]);
+			else if(i < 10) GUI.Button(new Rect(150, 20 + (60 * (i + 1)), 100, 40), userNames[i]);
+
+		}
+
+
+	}
 
 }
